3fe1a073b2646ab0ce06951bea5bb0f767d31457
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-05-24 07:34:29 -0700

    Create home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../components/category.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFEF6DB),
+      appBar: AppBar(
+        backgroundColor: Color(0xff46322B),
+        title: Text('Toku', style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        children: [
+          Category(
+            text: 'Numbers',
+            color: Colors.orange,
+          ),
+          Category(
+            text: 'Family Members',
+            color: Colors.green,
+          ),
+          Category(
+            text: 'Colors',
+            color: Colors.purple,
+          ),
+          Category(
+            text: 'Phrases',
+            color: Colors.lightBlue,
+          )
+        ],
+      ),
+    );
+  }
+}
+
