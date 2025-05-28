afb0f4131d191db5dd7c0f14e09c7a425d90cafa
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-05-28 13:40:08 -0700

    rename the name of the class and add an attribute

--- lib/models/number.dart
+++ lib/models/number.dart
@@ -1,9 +1,9 @@
 
 class itemModel{
-  final String image;
+  final String? image;
   final String jpName;
   final String enName;
   final String sound;
 
-  const itemModel({required this.image, required this.jpName, required this.enName, required this.sound});
+  const itemModel({this.image, required this.jpName, required this.enName, required this.sound});
 }
